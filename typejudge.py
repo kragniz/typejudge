45a1ba4c15533f3899f98839c7d69de41d710ff6
Louis Taylor <louis@kragniz.eu>
2016-07-28 13:05:15 +0100

    Judgement day

--- typejudge.py
+++ typejudge.py
@@ -4,33 +4,32 @@ import json
 import sys
 import typing
 
+from typing import Any, Dict, Callable
 
-def represent_type_hints_func(f):
+import semver
+
+
+def represent_type_hints_func(f: Callable) -> Dict[str, str]:
     d = {}
     for name, value in typing.get_type_hints(f).items():
         d.update({name: str(value)})
     return d
 
 
-def represent_type_hints_class(obj):
+def represent_type_hints_class(obj: Any) -> Dict[str, Dict]:
     d = {}
     for name, method in inspect.getmembers(obj):
         try:
-            d.update({name: represent_type_hints_func(method)})
+            hints = represent_type_hints_func(method)
+            if hints:
+                d.update({name: hints})
         except:
             pass
     return d
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='judge your types')
-    parser.add_argument('module', metavar='MODULE', default=None,
-                        help='module to import and check')
-    args = parser.parse_args()
-
-    m = __import__(args.module)
-
-    d = {}
+def represent_type_hints_module(module) -> Dict[str, Dict]:
+    types = {}
     for name, obj in m.__dict__.items():
         if inspect.isclass(obj):
             hints = represent_type_hints_class(obj)
@@ -40,5 +39,61 @@ if __name__ == '__main__':
             except AttributeError:
                 hints = {}
         if hints:
-            d.update({name: hints})
-    print(json.dumps(d, sort_keys=True, indent=4))
+            types.update({name: hints})
+
+    return types
+
+
+def judge(previous: Dict, current: Dict) -> str:
+    # ew this code right here someone rewrite it please
+    return_value = 0
+    for key, value in previous.items():
+        diff = 0
+        if isinstance(value, dict):
+            diff = judge(previous.get(key), current.get(key, {}))
+        else:
+            if value != current.get(key):
+                diff = 2
+
+        if diff > return_value:
+            return_value = diff
+
+    for key, value in current.items():
+        if key not in previous.keys():
+            if return_value == 0:
+                return_value = 1
+
+    return return_value
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='judge your types')
+    parser.add_argument('module', metavar='MODULE', default=None,
+                        help='module to import and check')
+    parser.add_argument('version', nargs='?', metavar='VERSION', default=None,
+                        help='current version of the package')
+    parser.add_argument('-o', '--out', default=None,
+                        help='save current type definitions to this file')
+    parser.add_argument('-f', '--file', default=None,
+                        help='load type definitions from this file')
+    args = parser.parse_args()
+
+    m = __import__(args.module)
+
+    previous_types = None
+    if args.file is not None:
+        with open(args.file) as f:
+            previous_types = json.load(f)
+
+    types = represent_type_hints_module(m)
+
+    if previous_types:
+        judgement = judge(previous_types, types)
+        print({0: 'patch', 1: 'minor', 2: 'major'}.get(0))
+
+    if args.out:
+        if args.out != '-':
+            with open(args.out, 'w') as f:
+                json.dump(types, f, sort_keys=True, indent=4)
+        else:
+            print(json.dumps(types, sort_keys=True, indent=4))
