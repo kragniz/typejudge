224ced7e71434bb10546ca409b36d5e0d0c4594f
Louis Taylor <louis@kragniz.eu>
2016-07-28 13:25:24 +0100

    Make pretty for github

---
+++ README.rst
@@ -0,0 +1,9 @@
+=========
+typejudge
+=========
+
+Judgement as a Service.
+
+Typejudge will automatically check for semver compliance based on type hints.
+If the type signature for a function changes, it will judge this to be an API
+change and recommend a major version bump.
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name="typejudge",
+    version="0.1.0",
+    url="https://github.com/kragniz/typejudge",
+
+    author="Louis Taylor",
+    author_email="louis@kragniz.eu",
+
+    description="Automatically check for semver compliance based on type hints",
+    long_description=open('README.rst').read(),
+
+    py_modules=['typejudge'],
+    entry_points={'console_scripts':
+        ['typejudge = typejudge:main',]
+    ,},
+
+    install_requires=[
+        "semver",
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
