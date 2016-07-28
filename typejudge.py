9fff30a334c785f676014f67cf7bb755488c71ca
Louis Taylor <louis@kragniz.eu>
2016-07-28 07:20:34 +0100

    Import modules for testing

--- typejudge.py
+++ typejudge.py
@@ -1,10 +1,13 @@
+import sys
 import inspect
 import typing
 
 import test
 
+
 def represent_type_hints_func(f):
     return str(typing.get_type_hints(f))
+
 
 def represent_type_hints_class(obj):
     d = {}
@@ -15,11 +18,16 @@ def represent_type_hints_class(obj):
             pass
     return d
 
-for f in [test.greeting, test.testing1, test.TestClass, test.TestClassNoInit]:
-    print(f)
-    if inspect.isclass(f):
-        r = represent_type_hints_class(f)
-    else:
-        r = represent_type_hints_func(f)
 
-    print(r)
+if __name__ == '__main__':
+    m = __import__(sys.argv[1])
+    for name, obj in m.__dict__.items():
+        if inspect.isclass(obj):
+            hints = represent_type_hints_class(obj)
+        else:
+            try:
+                hints = represent_type_hints_func(obj)
+            except AttributeError:
+                hints = {}
+        if hints:
+            print(name, obj, hints)
