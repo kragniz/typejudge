b062ef3ff3756e71d04ae71a15f26b48be0fb83d
Louis Taylor <louis@kragniz.eu>
2016-07-28 08:00:15 +0100

    Make some json

--- typejudge.py
+++ typejudge.py
@@ -1,12 +1,15 @@
-import sys
+import argparse
 import inspect
+import json
+import sys
 import typing
-
-import test
 
 
 def represent_type_hints_func(f):
-    return str(typing.get_type_hints(f))
+    d = {}
+    for name, value in typing.get_type_hints(f).items():
+        d.update({name: str(value)})
+    return d
 
 
 def represent_type_hints_class(obj):
@@ -20,7 +23,14 @@ def represent_type_hints_class(obj):
 
 
 if __name__ == '__main__':
-    m = __import__(sys.argv[1])
+    parser = argparse.ArgumentParser(description='judge your types')
+    parser.add_argument('module', metavar='MODULE', default=None,
+                        help='module to import and check')
+    args = parser.parse_args()
+
+    m = __import__(args.module)
+
+    d = {}
     for name, obj in m.__dict__.items():
         if inspect.isclass(obj):
             hints = represent_type_hints_class(obj)
@@ -30,4 +40,5 @@ if __name__ == '__main__':
             except AttributeError:
                 hints = {}
         if hints:
-            print(name, obj, hints)
+            d.update({name: hints})
+    print(json.dumps(d, sort_keys=True, indent=4))
