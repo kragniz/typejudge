7a43bac68403cde80a74a582e3ad8741e8a2ac6e
Louis Taylor <louis@kragniz.eu>
2016-07-30 09:34:23 +0100

    Release: 0.1.1 -> 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="typejudge",
-    version="0.1.1",
+    version="0.2.0",
     url="https://github.com/kragniz/typejudge",
 
     author="Louis Taylor",
